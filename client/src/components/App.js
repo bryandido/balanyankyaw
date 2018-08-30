d3ba7aecaf789e2d7eb0731fb1f2aaebe1d1b058
bryandido <balanyan.kyaw@sjsu.edu>
2018-08-29 19:09:53 -0700

    Added Payment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return <div>Hi There!</div>;
-};
+import Header from './Header';
+import Landing from './Landing';
 
-export default App;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Surveynew = () => <h2>SurveyNew</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact={true} path="/" component={Landing} />
+            <Route exact={true} path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={Surveynew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
